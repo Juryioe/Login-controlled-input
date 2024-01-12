496967a94887f360064eb9fa04a3c51e98e8b64e
Edgaras <e.jurgelevicius@gmail.com>
2024-01-12 18:49:37 +0200

    controlled-input

---
+++ src/components/Login.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+function Login() {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      userName,
+      password,
+    }
+
+    console.log(userData)
+  }
+
+  return (
+    <>
+      <h1>login form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  )
+}
+
+export default Login
